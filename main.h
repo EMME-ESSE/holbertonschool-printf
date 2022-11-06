94f81c39601cebbf224cbdb4a2c9bbaf9d8ba81c
EMME-ESSE <matimancini@gmail.com>
2022-11-06 12:14:51 -0800

    prototypes and libraries

--- main.h
+++ main.h
@@ -1,15 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stddef.h>
+#include <unistd.h>
 #include <string.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <limits.h>
 #include <stdarg.h>
+#include <limits.h>
 int _printf(const char *format, ...);
 int _putchar(char c);
-struct dato
-{
-	char *tipo;
-	char *cadena;
-};
 #endif
