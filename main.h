4596a11f268231538c39b28857739baff49ddb6f
EMME-ESSE <matimancini@gmail.com>
2022-11-07 11:32:07 -0800

    main.h folder

--- main.h
+++ main.h
@@ -6,6 +6,19 @@
 #include <stdio.h>
 #include <stdarg.h>
 #include <limits.h>
+
 int _printf(const char *format, ...);
 int _putchar(char c);
+
+int (*get_op_func(const char *s))(va_list arg);
+typedef struct op
+{
+	char *op;
+	int (*f)(va_list arg);
+} op_t;
+int op_i(char *);
+int op_d(char *);
+int op_mod(char *);
+int op_c(char *);
+int op_s(char *);
 #endif
