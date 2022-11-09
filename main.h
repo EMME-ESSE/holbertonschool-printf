e3f5de88ce41640cd12d0a0d5854f6b3d03fd8e3
EMME-ESSE <matimancini@gmail.com>
2022-11-09 06:10:21 -0800

    Definition of prototypes and structures

--- main.h
+++ main.h
@@ -1,24 +1,27 @@
 #ifndef MAIN_H
 #define MAIN_H
-#include <stddef.h>
-#include <unistd.h>
-#include <string.h>
-#include <stdio.h>
-#include <stdarg.h>
 #include <limits.h>
-
-int _printf(const char *format, ...);
-int _putchar(char c);
-
-int (*get_op_func(const char *s))(va_list arg);
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdlib.h>
+/**
+ *  *struct op - Struct used to print different operations.
+ *   *@op: Data selection.
+ *    *@f: Function to print;
+ *     **/
 typedef struct op
 {
-	char *op;
-	int (*f)(va_list arg);
+		char *op;
+			int (*f)(va_list arg);
 } op_t;
-int op_i(char *);
-int op_d(char *);
-int op_mod(char *);
-int op_c(char *);
-int op_s(char *);
+int op_c(va_list c);
+int op_s(va_list s);
+int op_d(va_list d);
+int op_i(va_list i);
+int _printf(const char *format, ...);
+int _putchar(char c);
+int (*get_op_func(const char *s))(va_list arg);
 #endif
